564b7be4ab7a27b143f9f5d061dfb6abe138a34d
Janberk <canberk.demirkan@googlemail.com>
2022-01-08 02:24:25 +0100

    feat: finish upload feature and course

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,21 +1,30 @@
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
+import validator from '@middy/validator';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
 
 async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer || 'Canberk';
+
   const auction = await getAuctionById(id);
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this auction!`);
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
+  let updatedAuction;
+
   try {
-    const uploadToS3Result = await uploadPictureToS3(
-      auction.id + '.jpg',
-      buffer,
-    );
-    console.log(uploadToS3Result);
+    const picturUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, picturUrl);
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
@@ -23,8 +32,17 @@ async function uploadAuctionPicture(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(
+    validator({
+      inputSchema: uploadAuctionPictureSchema,
+      ajvOptions: {
+        strict: false,
+      },
+    }),
+  );
